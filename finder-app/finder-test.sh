c5c04938782293b8bd44526a7894f1d292a73d60
4trastos <info@4trastos.com>
2025-10-14 13:52:14 +0200

    Fix absolute paths in finder-test.sh and add assignment4 result file

---
+++ finder-app/finder-test.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# Tester script for assignment 1 and assignment 2
+# Author: Siddhant Jajoo
+
+set -e
+set -u
+
+NUMFILES=10
+WRITESTR=AELD_IS_FUN
+WRITEDIR=/tmp/aeld-data
+
+# RUTAS ABSOLUTAS CORREGIDAS
+username=$(cat /etc/finder-app/conf/username.txt)
+
+if [ $# -lt 3 ]
+then
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi  
+else
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
+fi
+
+MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
+
+echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+
+rm -rf "${WRITEDIR}"
+
+# RUTA ABSOLUTA CORREGIDA
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
+
+if [ "$assignment" != 'assignment1' ]
+then
+    mkdir -p "$WRITEDIR"
+
+    if [ -d "$WRITEDIR" ]
+    then
+        echo "$WRITEDIR created"
+    else
+        exit 1
+    fi
+fi
+
+for i in $(seq 1 $NUMFILES)
+do
+    # COMANDO EN PATH (sin ./)
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+done
+
+# COMANDO EN PATH (sin ./)
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+# ESCRIBIR RESULTADO - AÑADIDO
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
+
+# remove temporary directories
+rm -rf /tmp/aeld-data
+
+set +e
+echo "${OUTPUTSTRING}" | grep "${MATCHSTR}"
+if [ $? -eq 0 ]; then
+    echo "success"
+    exit 0
+else
+    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
+fi
