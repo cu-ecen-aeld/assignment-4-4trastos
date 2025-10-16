2d1cdd5531085f891d92a642f42cef897f8dee90
4trastos <info@4trastos.com>
2025-10-16 09:12:56 +0200

    task-assignment-4-completed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,35 +1,43 @@
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# AESD-ASSIGNMENTS Buildroot package
 #
 ##############################################################
 
-# Versión/commit específico o 'master' para la última versión
-AESD_ASSIGNMENTS_VERSION = 'master'
-
-# URL del repositorio SSH (asegúrate de usar tu nombre de usuario)
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later.git'
+AESD_ASSIGNMENTS_VERSION = main
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-4trastos.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)"
 endef
 
+# Instalación de scripts y archivos de configuración
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
-    
-    $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-    $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-    
-    $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
-    $(INSTALL) -m 0644 $(@D)/finder-app/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/conf/
-    $(INSTALL) -m 0644 $(@D)/finder-app/conf/username.txt $(TARGET_DIR)/etc/finder-app/conf/
-    
-    $(if $(wildcard $(@D)/server/aesdsocket), \
-        $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin, \
-    )
+	# Crear directorios necesarios
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
+	
+	# Instalar archivos de configuración
+	if [ -f "$(@D)/conf/username.txt" ]; then \
+		$(INSTALL) -m 0644 $(@D)/conf/username.txt $(TARGET_DIR)/etc/finder-app/conf/; \
+	else \
+		echo "testuser" > $(TARGET_DIR)/etc/finder-app/conf/username.txt; \
+	fi
+	
+	if [ -f "$(@D)/conf/assignment.txt" ]; then \
+		$(INSTALL) -m 0644 $(@D)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/conf/; \
+	else \
+		echo "assignment4" > $(TARGET_DIR)/etc/finder-app/conf/assignment.txt; \
+	fi
+	
+	# Instalar binarios en /bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
+	
+	# Instalar scripts de test
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin/
 endef
 
 $(eval $(generic-package))
--- clean.sh
+++ clean.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-# Script to clean buildroot configuration
-# Author: Your Name
-
-set -e
-cd `dirname $0`
-
-echo "Cleaning buildroot..."
 make -C buildroot distclean
-
-echo "Removing generated configurations..."
-rm -f base_external/configs/aesd_qemu_defconfig
